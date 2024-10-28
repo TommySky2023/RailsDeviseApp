02f8a4b3d9678c62f8e757d45472e93c3c1fbcb2
DESKTOP-2STJP7I\user <tomoyuki.enomoto@gmail.com>
2024-10-29 08:08:22 +0900

    Deviseを導入した

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'hello/index'
+  get 'hello/login_check'
+  devise_for :accounts
+  
+  devise_scope :account do
+    get 'accounts/sign_out' => "devise/sessions#destroy"
+  end
 end
